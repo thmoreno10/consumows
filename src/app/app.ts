839d324b3d73f62f151ca3e9dbfb3eaa20ef2694
theomoreno10 <theomoren148@gmail.com>
2025-07-09 14:13:20 -0500

    cambios url

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
 
-export interface forumPost {
-  userId: number;
+export interface forumPost {  
   id: number,
-  title: string,
-  body: string;
+  name: string,
+  address: string,
+  phone: string
 }
 
 
